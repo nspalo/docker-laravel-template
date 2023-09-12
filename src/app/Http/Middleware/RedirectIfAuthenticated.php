2a93e44b1d1d0b3171264321330de79a372539f2
Noel Palo <noelspalo@gmail.com>
2023-09-12 22:49:12 +0800

    feature/DLT-29-add-coding-standards - Add PhpUnit - Add PhpStan - Add Easy Coding Standards

--- src/app/Http/Middleware/RedirectIfAuthenticated.php
+++ src/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -12,10 +12,10 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @param  string|null  ...$guards
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @param Closure $next
+     * @param ...$guards
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|mixed
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -26,14 +26,12 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+     protected $namespace = ''; // 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->configureRateLimiting();
 
